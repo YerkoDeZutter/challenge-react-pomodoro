3998c92813abc3760cd87706eb06db00763f592d
Yerko De Zutter <yerkodezutter@gmail.com>
2019-04-05 10:00:52 +0200

    deploiment test

---
+++ build/precache-manifest.af47d4577061d57e048e36acf351f89f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f971cbf4af2cd6b7a26d",
+    "url": "/challenge-react-pomodoro/static/js/runtime~main.0c2dd5de.js"
+  },
+  {
+    "revision": "e7f99d711210f3548fac",
+    "url": "/challenge-react-pomodoro/static/js/main.1e374041.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/challenge-react-pomodoro/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e7f99d711210f3548fac",
+    "url": "/challenge-react-pomodoro/static/css/main.7d9a74db.chunk.css"
+  },
+  {
+    "revision": "9a2fc82107d5baaf3f46de2bbb53d786",
+    "url": "/challenge-react-pomodoro/index.html"
+  }
+];
